b248adf733cf5ba723e3fabe29264b7babc9aa8f
Rick Vidal <rick@dtime.com>
2012-03-13 16:53:02 -0400

    Finish user signup

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     name     "Michael Hartl"
     email    "michael@example.com"
     password "foobar"
+    password_confirmation "foobar"
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,18 +4,25 @@ describe "User pages" do
 
   subject { page }
   
-  describe "signup page" do
-    
-    before { visit signup_path }
-        
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
+  describe "signup" do
+
+      before { visit signup_path }
+
     describe "with invalid information" do
       it "should not create a user" do
-        expect { click_button "Sign up" }.not_to change(User, :count)
+        expect { click_button "Create my account" }.not_to change(User, :count)
       end
     end
 
     describe "with valid information" do
-      
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
@@ -24,16 +31,10 @@ describe "User pages" do
       end
 
       it "should create a user" do
-        expect { click_button "Sign up" }.to change(User, :count).by(1)
+        expect do
+          click_button "Create my account"
+        end.to change(User, :count).by(1)
       end
     end
-  end
-  
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
   end
 end
