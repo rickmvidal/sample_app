cae2708183d247d70fee90bb87c88b94e4daad26
Rick Vidal <rick@dtime.com>
2012-03-15 17:01:24 -0400

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,9 @@
 module SessionsHelper
   
   def sign_in(user)
-      cookies.permanent[:remember_token] = user.remember_token
+      #cookies.permanent[:remember_token] = user.remember_token
       current_user = user
+      session[:user_id] = user.id
     end
     
     def signed_in?
@@ -14,18 +15,36 @@ module SessionsHelper
     end
     
     def current_user
-        @current_user ||= user_from_remember_token
+        @current_unser ||= User.find(session[:user_id])if session[:user_id]
+        #@current_user ||= user_from_remember_token
+    end
+    
+    def current_user?(user)
+      user == current_user
     end
     
     def sign_out
         current_user = nil
-        cookies.delete(:remember_token)
+        session[:user_id] = nil
+        #cookies.delete(:remember_token)
+    end
+    
+    def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      clear_return_to
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
     end
 
     private
 
-        def user_from_remember_token
-          remember_token = cookies[:remember_token]
-          User.find_by_remember_token(remember_token) unless remember_token.nil?
-        end
+      def clear_return_to
+        session.delete(:return_to)
+      end
+        #def user_from_remember_token
+          #remember_token = cookies[:remember_token]
+          #User.find_by_remember_token(remember_token) unless remember_token.nil?
+        #end
 end
